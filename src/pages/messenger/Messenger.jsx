32f7d80125722c8566435430f7fa3f6117b65c2c
nowhere132 <canh.manh123@gmail.com>
2021-11-15 23:39:33 +0700

    add chatEmpty & welcomePage

--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -1,15 +1,15 @@
-import { Col, Layout, Row, Switch } from 'antd';
+import { Col, Layout, Row } from 'antd';
 import MyHeader from '../../components/header/Header';
-import { Content, Header } from 'antd/lib/layout/layout';
-import Sider from 'antd/lib/layout/Sider';
 import { observer } from 'mobx-react-lite';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { useStore } from '../../hook';
 import Conversation from '../../components/conversation/Conversation';
+import ChatEmpty from '../../components/ChatEmpty/ChatEmpty';
 import './messenger.css';
 
 const Messenger = observer(() => {
   const AuthStore = useStore('AuthStore');
+  const { Sider, Content, Header } = Layout;
 
   return (
     <Layout className="app">
@@ -20,11 +20,13 @@ const Messenger = observer(() => {
         <Header>{`Wilina-${AuthStore.user?.username}`}</Header>
         <Content>
           <Row className="container" gutter={0}>
-            <Col span={6}><Conversation /></Col>
+            <Col span={6}>
+              <Conversation />
+            </Col>
             <Col span={18} className="container_chat_main">
               <Switch>
-                {/* <Route path="/messenger" exact component={ChatEmpty}/>
-                    <Route path="/messenger/:conversationId" exact component={ContrainerMess}/> */}
+                <Route path="/messenger" exact component={ChatEmpty} />
+                {/* <Route path="/messenger/:conversationId" exact component={ContrainerMess}/> */}
               </Switch>
             </Col>
           </Row>
